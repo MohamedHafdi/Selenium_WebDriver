c7e89abc5ed7633aea9c6a0281b73e5c618a6191
Mohamed Hafdi <mohamedhafdi14@gmail.com>
2025-04-09 21:13:18 +0000

    SubmitOrder update

--- src/test/java/MohamedHafdi/Tests/SubmitOrderTest.java
+++ src/test/java/MohamedHafdi/Tests/SubmitOrderTest.java
@@ -28,6 +28,7 @@ import MohamedHafdi.pageobjects.PaymentOrder;
 import MohamedHafdi.pageobjects.ProductCatalogue;
 
 //@Listeners(MohamedHafdi.TestComponents.Listeners.class)
+//Mohamed Hafdi check with GitHub WebHook
 public class SubmitOrderTest extends BaseTest {
 
 		
